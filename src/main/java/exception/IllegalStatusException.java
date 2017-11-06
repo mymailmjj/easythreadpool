2d565165100d3c094215191359c331bf51704dc5
mymailmjj <mymailmjj@yeah.net>
2017-11-06 17:59:33 +0800

    提交

---
+++ src/main/java/exception/IllegalStatusException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package exception;
+
+/**
+ * 线程池非法状态异常
+ * @author mujjiang
+ *
+ */
+public class IllegalStatusException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
